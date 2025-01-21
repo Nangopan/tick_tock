2a81607f43f652116754ec2406a7cf83d346a861
Nangopa <nangopan888@gmail.com>
2025-01-21 18:33:50 +0530

    FEAT:view engine setup

---
+++ controllers/user/userController.js
@@ -0,0 +1,26 @@
+
+
+const loadHomePage=async (req,res)=>{
+try{
+    return res.render("home")
+}
+catch(error){
+ console.log("Home page not found")
+ res.status(500).send("Server Error")
+}
+}
+
+const pageNotFound=async (req,res)=>{
+    try{
+        res.render('page-404')
+    }
+    catch(error){
+        res.redirect('/pageNotFound')
+    }
+}
+
+
+module.exports={
+    loadHomePage,
+    pageNotFound,
+}
